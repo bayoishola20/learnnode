8664623241f46f3a26783bfe64059ef8a870ebaf
bayoishola20 <bayoishola20@yahoo.com>
2017-08-28 11:20:48 +0100

    folder rename

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Everything looks good! Let\'s get going');
+  res.send('Everything looks good! Let\'s get going!!!');
 });
 
 module.exports = router;
