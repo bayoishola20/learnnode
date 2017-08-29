eb36fc3a798e034204ec9d74791c17c844371d79
bayoishola20 <bayoishola20@yahoo.com>
2017-08-29 23:13:12 +0100

    store-app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  // const human = { name: "Bayo", age: 2, location: "Nigeria" };
   res.send('Everything looks good! Let\'s get going!!!');
+  // res.json(human);
+  // res.json(req.query.name);
 });
 
 module.exports = router;
