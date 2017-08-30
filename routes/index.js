b7a3125f2121bf5309f19d6286f6c83999c299fa
bayoishola20 <bayoishola20@yahoo.com>
2017-08-30 08:29:34 +0100

    more of pug template engine and sending responses

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,22 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  // const human = { name: "Bayo", age: 2, location: "Nigeria" };
-  res.send('Everything looks good! Let\'s get going!!!');
-  // res.json(human);
-  // res.json(req.query.name);
+  //const human = { name: "Bayo", age: 2, location: "Nigeria" };
+  // res.send('Everything looks good! Let\'s get going!!!');
+  /*res.json(human);
+  res.json(req.query.name);
+  res.json(req.query);*/
+  res.render('hello', {
+    name: req.query.name,
+    age: 105,
+    title: 'Hello'
+  });
 });
+
+//reverses the request
+/*router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse);
+});*/
 
 module.exports = router;
