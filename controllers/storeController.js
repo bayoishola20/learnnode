8b9969829f9bbe9d8009687ae4d420608c227e93
bayoishola20 <bayoishola20@yahoo.com>
2017-08-30 13:11:13 +0100

    created store model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'bayo';
-    // res.cookie('name', 'bayo is awesome', {maxAge: 50000});
-    if(req.name === 'bayo') {
-        throw Error("What's that?");
-    }
-    next();
-}
-
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
---
+++ models/Store.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs'); //allows for url friendly names
+
+const storeSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        trim: true,
+        required: 'Please enter a valid store name.'
+    },
+    slug: String,
+    Description:{
+        type: String,
+        trim: true
+    },
+    tags: [String]
+});
+
+storeSchema.pre('save', (next) => {
+    if (!this.isModified('name')) {
+        next(); //Skip
+        return; //stops function from running further
+    }
+    this.slug = slug(this.name);
+    next();
+    //TO DO: Handle uniqueness
+});
+
+module.exports = mongoose.model('Store', storeSchema)
