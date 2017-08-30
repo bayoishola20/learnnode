f2ebddf79d4892e58de0325ac03cbccd39b804e9
bayoishola20 <bayoishola20@yahoo.com>
2017-08-30 09:06:46 +0100

    controllers creation

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  //const human = { name: "Bayo", age: 2, location: "Nigeria" };
-  // res.send('Everything looks good! Let\'s get going!!!');
-  /*res.json(human);
-  res.json(req.query.name);
-  res.json(req.query);*/
-  res.render('hello', {
-    name: req.query.name,
-    age: 105,
-    title: 'Hello'
-  });
-});
+router.get('/', storeController.homePage);
 
 //reverses the request
 /*router.get('/reverse/:name', (req, res) => {
