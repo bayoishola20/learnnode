c52ff43c83b6fdc125ed6a082cbfccbf46bc8258
bayoishola20 <bayoishola20@yahoo.com>
2017-09-06 05:42:47 +0100

    Tags aggregation -> mongodb aggregation

--- models/Store.js
+++ models/Store.js
@@ -45,14 +45,22 @@ storeSchema.pre('save', async function(next) {
     //TO DO: Handle slug uniqueness
     const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
 
-    const storeWithSlug =  await this.constructor.find({ slug: slugRegEx }); //this.constructor will return "store"
+    const storesWithSlug =  await this.constructor.find({ slug: slugRegEx }); //this.constructor will return "store"
 
-    if(storeWithSlug.length) {
-        this.slug = `{this.slug}-{storesWithSlug.length + 1}`
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
     }
-
-
     next();
 });
+
+//Add static method
+
+storeSchema.statics.getTagsList = function () {
+    return this.aggregate([
+        { $unwind: '$tags' },
+        { $group: {_id: '$tags', count: { $sum: 1 }}},
+        {$sort: { count: -1 } }
+    ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema)
