3ef805c21b60718e88bbac00d0cadd5dbe65fa8a
bayoishola20 <bayoishola20@yahoo.com>
2017-09-08 14:13:06 +0100

    login post route fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,3 +8,9 @@ exports.login = passport.authenticate('local', {
     successRedirect: '/',
     successFlash: 'Logged in'
 });
+
+exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', 'You are now logged out');
+    res.redirect('/');
+}
